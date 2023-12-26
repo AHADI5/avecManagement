6e2676d82781718b4646952a5cd7134746885e8b
= <=>
2023-12-26 01:42:24 +0100

    Menu , title-bar design UI html /css

--- views/include/menu.php
+++ views/include/menu.php
@@ -1,39 +1,51 @@
 <div class="menu-content">
-    <div class="logo"></div>
-    <div class="menucontent">
+    <div class="logo">
+        <div class = "log-text">G-AvM</div>   
+    </div>
+    <hr>
+
+    
+    <div class="menucontent flex">
         <ul>
             <li>
                 <a href="">
+                   <div class="icon"> <i class = "bi bi-house-door"></i></div>
                     <div class="menu">Home</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <div class="icon"><i class="bi bi-collection"></i></div>
                     <div class="menu">Avec</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                   <div class="icon"> <i class="bi bi-people-fill"></i></div>
                     <div class="menu">Membres</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <div class="icon">  <i class="bi bi-credit-card-fill"></i></div>
                     <div class="menu">Credits</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <div class="icon"><i class="bi bi-cash-coin"></i></div>
                     <div class="menu">Epargnes</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <div class="icon"> <i class="bi bi-calendar-heart-fill"></i></div>
                     <div class="menu">Social</div>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <div class="icon"> <i class="bi bi-back"></i></div>
                     <div class="menu">Remb</div>
                 </a>
             </li>
--- views/include/titlePage.php
+++ views/include/titlePage.php
@@ -1,9 +1,12 @@
 <div class="title-bar flex">
-    <div class="title-app">AVEC Management</div>
+    <div class="title-app flex">AVEC Management</div>
     <div class="settings flex">
         <div class="notifications"><i class = "bi bi-bell"></i></div>
         <div class="profile-section flex">
-            <div class="profile-pic"><i class = "bi bi-person-circle" ></i> </div>
+            <div class="name-pic flex">
+                <div class="profile-pic"><i class = "bi bi-person-circle" ></i> </div>
+                <div class="name">Admin</div>
+            </div>
             <div class="drop-down"><i class = "bi bi-chevron-down"></i></div>
         </div>
     </div>
